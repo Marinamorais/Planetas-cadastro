a316efd4c209a40c1f3b0291698fbd24af3e98bb
Marinamorais <Marinamorais.564@gmail.com>
2024-03-23 18:28:30 -0300

    modificando a home

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,22 +1,17 @@
-import { View } from "react-native";
+import { Text, View } from "react-native";
 
 import styles from "./styles";
 import Title from "../../components/Title";
-import { user } from "../../data/Profile";
-import TouchButton from "../../components/TouchButton";
 
 export default function Home() {
   return (
     <View style={styles.container}>
       <Title title="Home" />
 
-      <TouchButton route="Profile" title="Go to Profile" data={user} />
-
-      <TouchButton route="Planets" title="Go to Planets" />
-
-      <TouchButton route="Users" title="Go to Users" />
-
-      <TouchButton route="UsersList" title="Go to Users List" />
+      <View>
+        <Text>Tela de apresentação do app</Text>
+      </View>
     </View>
   );
 }
+
