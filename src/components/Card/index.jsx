cedfacb9b3971a355088321fe56d422b9fa977af
Marinamorais <marinamorais@aluno.senai.br>
2024-03-25 16:59:18 -0300

    alterando os card

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,19 +1,15 @@
-import React from 'react';
-import { View, Text, Image } from 'react-native';
-import styles from './styles';
+import React from "react";
+import { View, Text, Image } from "react-native";
+import styles from "./styles";
 
-
-const Card = ({ title, image, }) => {
-    return (
-        <View style={styles.container}>
-        <Image source={image} style={styles.image} />
-        <Text style={styles.title}>{title}</Text>
-        <Text style={styles.info}>Data de descobrimento : XVI e XVII</Text>
-        <Text style={styles.info}>Cor: Rosa</Text>
-        <Text style={styles.info}>Cor seguntaria : Azul</Text>
-
-        </View>
-    );
-}
+const Card = ({ title, image, descri }) => {
+  return (
+    <View style={styles.container}>
+      <Image source={image} style={styles.image} />
+      <Text style={styles.title}>{title}</Text>
+      <Text style={styles.info}>{descri}</Text>
+    </View>
+  );
+};
 
 export default Card;
