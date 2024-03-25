c240346e8e2ec688b2eb675762e1feb412335e99
Marinamorais <marinamorais@aluno.senai.br>
2024-03-25 17:10:30 -0300

    header components

---
+++ src/components/Header/styles.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  header: {
+    backgroundColor: "#051219",
+    width: "100%",
+    height: 80,
+    alignItems: "center",
+  },
+  logo: {
+    width: 100,
+    height: 100,
+    marginBottom: 50,
+  },
+});
